b7fe447a81290dcca8c027d87741f4ce8d1bbc56
gonzalez-lourdnathaniel <gonzalez.lourdnathaniel@gmail.com>
2023-11-13 20:29:08 +0800

    Created model schema

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,5 +1,32 @@
 const mongoose = require('mongoose');
 
-const teacherSchema = new mongoose.Schema({});
+const teacherSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name required."],
+    },
+    middleName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name required."]
+    },
+    email: {
+        type: String,
+        required: [true, "Email required."]
+    },
+    password: {
+        type: String,
+        required: [true, "Password required."]
+    },
+    role: {
+        type: String,
+        default: "teacher",
+    },
+}, {
+    timestamps: true,
+    versionKey: false,
+});
 
 module.exports = mongoose.model("Teacher", teacherSchema);
